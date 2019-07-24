f5c9529cedb1e2679a876ee8da7c9c2ba6f5446d
Stefan Dailoski <dailoskistefan@gmail.com>
2019-07-24 11:04:05 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import HorizontalNonLinearAlternativeLabelStepper from './Form';
+
+function App() {
+  return (
+    <div className="App">
+      <HorizontalNonLinearAlternativeLabelStepper />
+    </div>
+  );
+}
+
+export default App;
